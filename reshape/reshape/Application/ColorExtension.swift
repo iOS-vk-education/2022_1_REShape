178d262a1c6fc88e56a5dbb73d196f892e0ed8c3
Egor Shkarin <58490727+Grandschtien@users.noreply.github.com>
2022-04-14 13:16:19 +0300

    Made some fixes after merge
    Икскод потерял пару файлом после исправления конфликтов, все вернул обратно

---
+++ reshape/reshape/Application/ColorExtension.swift
@@ -0,0 +1,22 @@
+//
+//  ColorExtension.swift
+//  reshape
+//
+//  Created by Егор Шкарин on 14.04.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let blueColor = UIColor(named: "Blue")
+    static let greenColor = UIColor(named: "Green")
+    static let lightGrayColor = UIColor(named: "LightGray")
+    static let modalViewGrayColor = UIColor(named: "ModalViewColor")
+    static let orangeColor = UIColor(named: "Orange")
+    static let shadowColor = UIColor(named: "Shadow")
+    static let backgroundGrayColor = UIColor(named: "TableViewBackground")
+    static let violetColor = UIColor(named: "Violet")
+    static let darkVioletColor = UIColor(named: "VioletPressed")
+    static let blackVioletColor = UIColor(named: "Dark Violet")
+    static let pureGreyColor = UIColor(named: "Grey for dietScreen")
+}
